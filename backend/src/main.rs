f4a5224d38f2ff8ba4063b1173bd88e56d746191
cleopha <coline@seguret.net>
2023-07-04 16:20:12 +0200

    feat: simple test and ci build, test, lint
    Signed-off-by: cleopha <coline@seguret.net>

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    print!("Hello World! {:?}", simple_test())
 }
 
+fn simple_test() -> u16 {
+    12
+}
+
+#[test]
+fn feature() {
+    assert_eq!(simple_test(), 12);
+}
